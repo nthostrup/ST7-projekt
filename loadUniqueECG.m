615e25968d4f8cb1d96b85fb42c64c739d270b8b
Nicolaj <nthost15@student.aau.dk>
2020-10-21 10:27:26 +0200

    Function to load XML files from directory and return unique person ECGs

---
+++ loadUniqueECG.m
@@ -0,0 +1,35 @@
+function [uniqueECGs] = loadUniqueECG(directorypath)
+%loadUniqueECG Summary of this function goes here
+%  Inputs:
+%   dir: Directory of data to load
+%  Outputs
+%   UniqueECGs: Array with serial ECG for each person and datetime stamp
+
+xmlfiles = dir(fullfile(directorypath,'*xml'));
+personIDArray = char(zeros(length(xmlfiles),10));
+
+for i=1:length(xmlfiles)
+    XML(i) = XMLECGParser(xmlfiles(i).name);
+    personIDArray(i,:) = XML(i).TestInfo.PatientID;  
+end
+
+
+uniqueIDs = unique(personIDArray,'rows');
+
+uniqueECGs = struct('ID','','ECGs',0,'dateTimeAcq','','fileName','');
+for j=1:size(uniqueIDs,1)
+    ID = uniqueIDs(j,:);
+    uniqueECGs(j).ID=ID;
+    k = 1;
+    for i=1:length(XML)
+        if(XML(i).TestInfo.PatientID == ID)
+            uniqueECGs(j).ECGs(k,1:600,1:12) = XML(i).MedianECG.ECG12Leads;
+            uniqueECGs(j).dateTimeAcq(k,:) = XML(i).TestInfo.AcqDateTime;
+            uniqueECGs(j).fileName(k,:) = XML(i).TestInfo.Filename;
+            k = k+1;
+        end
+    end
+end
+
+end
+
