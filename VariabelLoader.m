2185da506ce26773897ed17462475cda29b5727f
Frederik <fsamue17@student.aau.dk>
2020-10-28 14:44:15 +0100

    ændring så alle variabler spyttes ud på én gang

--- VariabelLoader.m
+++ VariabelLoader.m
@@ -4,8 +4,9 @@
 
 %load() %% insert file
 
-%% beregn nu p-off
-XML = aIAB_XML_loaded;          
+    % beregn nu p-off
+%XML = noIAB; 
+disp('starter')
 for i=1:length(XML)
     %Angiv den konkrete fil der skal køres i scriptet ved index af xmlfiler
     %XML = XMLECGParser(xmlfiles(i).name);
@@ -24,8 +25,8 @@ for i=1:length(XML)
         Off(i)=calcPoff(infECG,GEOff,Qon);
     end
 end
-
-%% transformaer til VCG 
+disp('p_offset er beregnet')
+    % transformaer til VCG 
 
 %Kors regression
 T=[-0.130 0.050 -0.010 0.140 0.060 0.540 0.380 -0.07;
@@ -39,17 +40,22 @@ ecg = [XML(i).MedianECG.V1 XML(i).MedianECG.V2 XML(i).MedianECG.V3 XML(i).Median
 
 VCG_T(i,:,:) = (ecg*T); %transformed VCG
 end 
-
-%% beregn pseudoleads calc
+disp('VCG er beregnet')
+    % beregn pseudoleads calc
 
 for i=1:1:length(XML)
    [leadprojlength, Pseudo_electrodes(i,:,:), numerationOfElectrodes, PC12_ratio(i)] = PseudoLeadsCalc(squeeze(VCG_T(i,On(i):Off(i),:)));   
 [p_iab(i), biphasic_p_wave(i,:), sum_p_loop(i,:), sum_p_inv_loop(i,:), a(i,:), b(i,:)] = detectionFile(leadprojlength', 1, length(leadprojlength));   
 end
-
-
-%% beregning af LoadUniqueEcgs
-
-loadUnique
+disp('detektion af pseudoleads er beregnet')
+    % Beregn biphasic p-wave for de konventionelle ecg'er
+for i =1:1:length(XML)
+infECG=[XML(i).MedianECG.II,XML(i).MedianECG.aVF,XML(i).MedianECG.III]; %infECG er midlertidig
+[konv_p_iab(i), konv_biphasic_p_wave(i,:), konv_sum_p_loop(i,:), konv_sum_p_inv_loop(i,:), konv_a(i,:), konv_b(i,:)] = detectionFile(infECG, On(i), Off(i));   
+end 
+disp('detektion af konv. inf leads er beregnet')
+    % beregning af LoadUniqueEcgs
+uniqueECGS=loadUniqueECG_from_XML(XML);
+disp('Færdig')
 
 
