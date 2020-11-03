2f0359dcdd3975045a25596a82016e544121fd4e
Frederik <fsamue17@student.aau.dk>
2020-11-03 08:35:16 +0100

    Rettelse af funktion. Funktionen virkede ikke før.

--- amplitudeDetectionMethod.m
+++ amplitudeDetectionMethod.m
@@ -8,7 +8,7 @@ for i=1:length(PprimeAmp)
     if PprimeAmp(i,1) < 0
     PprimeAmp(i,1) = PprimeAmp(i,1)*-1;         %%Sørger for at amplituden er positivt
     end 
-    if min(PprimeAmp(i,1)) < threshold     %mikroV
+    if max(PprimeAmp(i,1)) > threshold     %mikroV %%%%%% kig HER. har ændret fra min(PprimeAmp(i,1)) < threshold -> synes ikke det giver mening
     detectionOutput(i) = 1;
     else
     detectionOutput(i) = 0;
