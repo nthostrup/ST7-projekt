75ff7d17a9ee6fcd37d2a4c33d139b260450e9b4
Frederik <fsamue17@student.aau.dk>
2020-11-03 09:53:09 +0100

    Rettelser af beregning for threshold

--- amplitudeDetectionMethod.m
+++ amplitudeDetectionMethod.m
@@ -1,6 +1,6 @@
 function [detectionOutput] = amplitudeDetectionMethod(PprimeAmp, threshold)
 % detectionOutput er en variabel, som med “1” eller “0” indikerer om metoden har fundet den givne karakteristika.
-% PprimeAmp: Array med amplitude målt i de 18 pseudo leads.
+% PprimeAmp: Array med amplitude målt i P0, for hver person.
 % Threshold: threshold for amplituden, skal være positiv
 
 
@@ -8,7 +8,7 @@ for i=1:length(PprimeAmp)
     if PprimeAmp(i,1) < 0
     PprimeAmp(i,1) = PprimeAmp(i,1)*-1;         %%Sørger for at amplituden er positivt
     end 
-    if max(PprimeAmp(i,1)) > threshold     %mikroV %%%%%% kig HER. har ændret fra min(PprimeAmp(i,1)) < threshold -> synes ikke det giver mening
+    if PprimeAmp(i,1) > threshold     
     detectionOutput(i) = 1;
     else
     detectionOutput(i) = 0;
