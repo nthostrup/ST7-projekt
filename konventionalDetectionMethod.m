e4808937a9d6a470e9450c8b15fe7744d021f566
Maria <mmalle17@student.aau.dk>
2020-11-09 10:48:10 +0100

    New function to conventional leads

---
+++ konventionalDetectionMethod.m
@@ -0,0 +1,11 @@
+function [detectionOutput] = konventionalDetectionMethod(konv_biphasic_p_wave,  konv_p_iab)
+
+
+for i=1:length(konv_biphasic_p_wave)
+    if konv_biphasic_p_wave(i,1) == 1 && konv_biphasic_p_wave(i,2) == 1 && konv_biphasic_p_wave(i,3) == 1 && konv_p_iab(i) == 1
+    detectionOutput(i) = 1;
+    else
+    detectionOutput(i) = 0;
+    end
+    
+end
